abcf3ed1871adab3e6eb0770fce9329ee5afc42d
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-01-19 19:37:25 -0800

    see all comments

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ var exphbs = require('express-handlebars');
 // handlebars engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+// OUR MOCK ARRAY OF PROJECTS
+let comments = [
+  { title: "Great Review", content: "content" },
+  { title: "Awesome Movie", content: "content"}
+]
 
-
+// INDEX
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Qonday bolyappa'});
+  res.render('comments-index', { comments: comments });
 })
+
 
 app.listen(port, () => {
     console.log('App listening on port 3000!')
